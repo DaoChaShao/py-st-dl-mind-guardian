090ad12062b9aee46d2d2ff704cedaefb133d5f0
DaoChaShao <27882272@qq.com>
2026-07-17 23:09:57 +1200

    feat(tools): initialize tools module for Streamlit application
    - Add __init__.py with module metadata and documentation
    - Import and expose layout utilities (config_page, set_pages)
    - Define __all__ to control module exports
    - Set up author and version information
    - Configure module docstring with description and usage details
    - Initialize package

---
+++ app/tools/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2026/7/17 23:02
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   __init__.py.py
+# @Desc     :   
+
+"""
+****************************************************************
+Tools for Streamlit Application Subpages
+----------------------------------------------------------------
+A collection of utility tools designed for Streamlit page initialisation and core function execution.
+****************************************************************
+"""
+
+__author__ = "Shawn Yu"
+__version__ = "0.1.0"
+
+from .layout import config_page, set_pages
+
+__all__ = [
+    "config_page",
+    "set_pages",
+]
