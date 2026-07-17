7820a8a6fb83a7c87c741e6d92f7f2ca8fb9b973
DaoChaShao <27882272@qq.com>
2026-07-17 23:10:10 +1200

    feat(home): add home page with introduction sections
    - Create new home.py page with streamlit components
    - Add empty message to display core functions reminder
    - Implement title for Mind Guardian application
    - Add expandable introduction sections in Chinese and English
    - Include caption placeholders for introduction content
    - Set default expansion state for English introduction section

---
+++ app/subpages/home.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2026/7/17 23:03
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   home.py
+# @Desc     :   
+
+from streamlit import title, expander, caption, empty
+
+empty_message = empty()
+empty_message.info("Please check the details at the different pages of core functions.")
+
+title("Mind Guardian")
+with expander("**INTRODUCTION (CN)**", expanded=False):
+    caption("+ ")
+
+with expander("**INTRODUCTION (EN)**", expanded=True):
+    caption("+ ")
